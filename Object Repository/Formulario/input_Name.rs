fcb43396f644821ae355fe6d732e222ae3777363
Saganom3 <Saga3692>
2023-03-12 18:44:55 -0500

    Hola

---
+++ Object Repository/Formulario/input_Name.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Name</name>
+   <tag></tag>
+   <elementGuidId>5ae0eec8-8e67-4ce7-a98c-f0694fdb137c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'firstName']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>firstName</value>
+      <webElementGuid>2ed2dcd2-141e-4974-9ad7-1e8b6eeacac9</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
